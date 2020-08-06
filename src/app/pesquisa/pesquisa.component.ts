250841e7573927527f96793dc834a31390a594ad
root <rafa.liotti@gmail.com>
2020-08-06 12:02:38 -0300

    Terceiro commit - teste de alterações

--- src/app/pesquisa/pesquisa.component.ts
+++ src/app/pesquisa/pesquisa.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,18 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesquisaComponent {
 
+  nome = 'Testando';
+
+  nome2 = '';
+
+  adicionarTeste(nomeTeste : any){
+
+   this.nome = nomeTeste;
+
+  }
+
+  adicionar(){
+    const numero = Math.round(Math.random() * 100);
+
+    this.nome = 'Rafael ' + numero;
+
+    console.log('Estamos testando ' + this.nome);
+
+
+  }
+
+ // excluirItem(produto: any){
+ //   this.produto
+ // }
+
+
+  alterarNome(evento : any){
+
+    console.log(evento.target.value);
+
+    this.nome2 = evento.target.value;
+  }
+
+
+
 
   produtos = [
 
-    {nome: 'Toddy',
-     descricao: 'Achocolatado em Pó TODDY Original 800g',
-     tipo:'Alimento',
-     valor: '10.00'},
+    {
+      id: 1,
+      nome: 'Toddy',
+      descricao: 'Achocolatado em Pó TODDY Original 800g',
+      tipo:'Alimento',
+      valor: '10.00'
+    },
 
-     {nome: 'Chocolicia',
-     descricao: 'Biscoito Recheado Chocolate Chocolícia 143g',
-     tipo:'Alimento',
-     valor: '3.00'},
+    {
+      id: 2,
+      nome: 'Chocolicia',
+      descricao: 'Biscoito Recheado Chocolate Chocolícia 143g',
+      tipo:'Alimento',
+      valor: '3.00'
+    },
   ];
 
 
